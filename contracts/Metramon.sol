a3fabf8ad921d14ba461264a7cb03769f5e6b1ad
Ittner <patrick.ittner@wechselgott.com>
2022-03-26 18:41:07 +0100

    add functionality to approve contract by opensea and transfering tokens by opensea

--- contracts/Metramon.sol
+++ contracts/Metramon.sol
@@ -3,42 +3,83 @@ pragma solidity ^0.8.12;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/security/PullPayment.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MetramonAi is ERC721, PullPayment {
+/**
+ * https://github.com/maticnetwork/pos-portal/blob/master/contracts/common/ContextMixin.sol
+ */
+abstract contract ContextMixin {
+    function msgSender()
+    internal
+    view
+    returns (address payable sender)
+    {
+        if (msg.sender == address(this)) {
+            bytes memory array = msg.data;
+            uint256 index = msg.data.length;
+            assembly {
+            // Load the 32 bytes word from memory with the address on the lower 20 bytes, and mask those.
+                sender := and(
+                mload(add(array, index)),
+                0xffffffffffffffffffffffffffffffffffffffff
+                )
+            }
+        } else {
+            sender = payable(msg.sender);
+        }
+        return sender;
+    }
+}
+
+contract Metramon is ContextMixin, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
     // Constants
-    uint256 public constant TOTAL_SUPPLY = 1000;
-    uint256 public constant MINT_PRICE = 0.01 ether;
+    uint256 public constant TOTAL_SUPPLY = 10001;
 
-    Counters.Counter private currentTokenId;
-
-    /// @dev Base token URI used as a prefix by tokenURI().
-    string public baseTokenURI;
-
-    constructor() ERC721("MetramonAi", "NFT") {
-        baseTokenURI = "";
+    constructor (string memory name_, string memory symbol_) ERC721(name_, symbol_) {
     }
 
-    function mintTo(address recipient) public payable returns (uint256) {
-        uint256 tokenId = currentTokenId.current();
+    /**
+     * This is used instead of msg.sender as transactions won't be sent by the original token owner, but by OpenSea.
+     */
+    function _msgSender()
+    internal
+    override
+    view
+    returns (address sender)
+    {
+        return ContextMixin.msgSender();
+    }
+
+    /**
+    * As another option for supporting trading without requiring meta transactions, override isApprovedForAll to whitelist OpenSea proxy accounts on Matic
+    */
+    function isApprovedForAll(
+        address _owner,
+        address _operator
+    ) public override view returns (bool isOperator) {
+        if (_operator == address(0x58807baD0B376efc12F5AD86aAc70E78ed67deaE)) {
+            return true;
+        }
+
+        return ERC721.isApprovedForAll(_owner, _operator);
+    }
+
+    function mintNFT(address recipient, string memory tokenURI)
+    public onlyOwner
+    returns (uint256)
+    {
+        uint256 tokenId = _tokenIds.current();
+        _tokenIds.increment();
         require(tokenId < TOTAL_SUPPLY, "Max supply reached");
-        require(msg.value == MINT_PRICE, "Transaction value did not equal the mint price");
 
-        currentTokenId.increment();
-        uint256 newItemId = currentTokenId.current();
-        _safeMint(recipient, newItemId);
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
         return newItemId;
-    }
-
-    /// @dev Returns an URI for a given token ID
-    function _baseURI() internal view virtual override returns (string memory) {
-        return baseTokenURI;
-    }
-
-    /// @dev Sets the base token URI prefix.
-    function setBaseTokenURI(string memory _baseTokenURI) public {
-        baseTokenURI = _baseTokenURI;
     }
 }
